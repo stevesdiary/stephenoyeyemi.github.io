f59c41e00d2ccb9e479d9cf90ff508ba0187e29b
steve-aella <stephenoyeyemi@gmail.com>
2024-09-14 19:56:03 +0100

    Initialise Github portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const yearSpan = document.getElementById('year');
+  const currentYear = new Date().getFullYear();
+  yearSpan.textContent = currentYear;
+});
