48c53ebc4954e9e9d7ba403bb54529c5b451c15f
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-07-21 08:43:45 +0100

    Delete old files

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const yearSpan = document.getElementById('year');
-  const currentYear = new Date().getFullYear();
-  yearSpan.textContent = currentYear;
-});
